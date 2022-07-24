542be7d19d30211704a7dc078ba80f7633f0d39b
carlinhocr <carlos.rienzi@gmail.com>
2022-07-23 21:33:37 -0300

    con Item 3 Control Flow

---
+++ Codeacademy_LearnPython3/02_ControlFlow.py
@@ -0,0 +1,13 @@
+
+
+def item_3():
+        print((5 * 2) - 1 == 8 + 1)
+        print(13 - 6 != (3 * 2) + 1)
+        print(3 * (2 - 1) == 4 - 1)
+
+def main():
+
+    print("Corriendo item 3", item_3())
+
+if __name__ == "__main__":
+    main()
